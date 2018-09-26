e9e729a5ca58b1ea55ac50390a221bd31e4a977b
KaurSamuel <33308949+KaurSamuel@users.noreply.github.com>
2018-09-26 22:19:37 +0300

    initial

---
+++ Tip calculator/Tip calculator/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace Tip_calculator
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView Tip_toolbar;
+        SeekBar Tip_seekbar;
+        TextView Result_textBox;
+        TextView bill_edit_text;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            bill_edit_text = FindViewById<EditText>(Resource.Id.Bill_editText);
+            Tip_seekbar = FindViewById<SeekBar>(Resource.Id.Tip_seek_bar);
+            Result_textBox = FindViewById<TextView>(Resource.Id.Result_text_box);
+            Tip_toolbar = FindViewById<TextView>(Resource.Id.Tip_toolbar);
+            Button Uptate_putton = FindViewById<Button>(Resource.Id.Uptate_button);
+            Uptate_putton.Click += delegate
+            {
+                Button_press();
+            };
+            Tip_seekbar.ProgressChanged += Tip_seekbar_ProgressChanged;
+        }
+
+        private void Tip_seekbar_ProgressChanged(object sender, SeekBar.ProgressChangedEventArgs e)
+        {
+
+            Tip_toolbar.Text = (e.Progress.ToString()+"%");
+            int result = int.Parse(bill_edit_text.Text) * int.Parse(Tip_seekbar.Progress.ToString()) / 100;
+            Result_textBox.Text = result.ToString();
+        }
+        private void Button_press ()
+        {
+            int result = int.Parse(bill_edit_text.Text) * int.Parse(Tip_seekbar.Progress.ToString()) / 100;
+            Result_textBox.Text = result.ToString();
+        }
+    }
+}
---
+++ Tip calculator/Tip calculator/obj/Debug/MonoAndroid81/android/src/md5878ea6c792561c287b1bd69dc08a879b/MainActivity.java
@@ -0,0 +1,47 @@
+package md5878ea6c792561c287b1bd69dc08a879b;
+
+
+public class MainActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Tip_calculator.MainActivity, Tip calculator", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Tip_calculator.MainActivity, Tip calculator", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
